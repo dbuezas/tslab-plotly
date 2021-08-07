0092cd01605ff64b00dc2c573bf323ac44da0e74
David Buezas <david.buezas@gmail.com>
2021-08-07 15:57:11 +0200

    Avoid multiple plotly bundles to be loaded

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,47 @@
 // David Buezas
 
-// type Plotly = typeof import("plotly.js");
+type TPlotly = typeof import("plotly.js");
 type Tslab = typeof import("tslab");
+
+declare const window: {
+  PlotlyPromise?: Promise<void>;
+  define?: (...args: any[]) => void;
+  Plotly?: TPlotly;
+};
+
+function ensurePlotly() {
+  if (!window.PlotlyPromise) {
+    const define = window.define;
+    window.define = undefined; // avoid plotly thinking it is in a requirejs environment (vscode)
+    const script = document.createElement("script");
+    script.setAttribute("type", "text/javascript");
+    script.setAttribute("id", "plotly-lib-bundle");
+    script.setAttribute("src", "https://cdn.plot.ly/plotly-latest.min.js");
+    window.PlotlyPromise = new Promise((resolve, reject) => {
+      script.onload = () => resolve();
+      script.onerror = () => reject(new Error("Error loading " + script.getAttribute("src")));
+  });
+    document.head.appendChild(script);
+    window.PlotlyPromise.catch().then(() => {
+      window.define = define;
+    });
+  }
+}
+
+async function runPlotlyScript(
+  plotId: string,
+  data: Plotly.Data[],
+  layout?: Partial<Plotly.Layout>,
+  config?: Partial<Plotly.Config>
+) {
+  const plotContainer = document.getElementById(plotId)!;
+  try {
+    await window.PlotlyPromise
+    window.Plotly!.newPlot(plotContainer, data, layout, config);
+  } catch (e) {
+    plotContainer.innerText = e.message + "\\n" + e.stack;
+  }
+}
 
 const newPlot = (
   tslab: Tslab,
@@ -24,39 +64,16 @@ const newPlot = (
         style="width:${width}px;height:${height}px;">
       </div>
       <script>
-        async function runPlotlyScript(){
-          if (!window.Plotly){
-            const define = window.define
-            window.define = undefined // avoid plotly thinking it is in a requirejs environment (vscode)
-            const script = document.createElement('script');
-            script.setAttribute('type', 'text/javascript');
-            script.setAttribute('id', 'plotly-lib-bundle');
-            script.setAttribute('src', 'https://cdn.plot.ly/plotly-latest.min.js');
-            const loadPlotly = new Promise((resolve, reject)=>{
-              script.onload = resolve;
-              script.onerror = reject;
-            })
-            document.head.appendChild(script);
-            loadPlotly.catch().then(() => {
-              window.define = define;
-            });
-            await loadPlotly;
-          }
-          try {
-            if (!window.Plotly){
-              throw new Error("Couldn't load Plotly from CDN");
-            }
-            const plotContainer = document.getElementById("${plotId}")
-            const args = ${JSON.stringify([data, layout, config])};
-            window.Plotly.newPlot(plotContainer,...args)
-          } catch (e){
-            plotContainer.innerText = e.message + '\\n' + e.stack
-          }
-        }  
-        runPlotlyScript();
+      {
+        ${ensurePlotly.toString()}
+        ${runPlotlyScript.toString()}
+
+        ensurePlotly();
+        runPlotlyScript(...${JSON.stringify([plotId, data, layout, config])});
+      }
       </script>
     `;
-    tslab.display.html(html);
-}
+  tslab.display.html(html);
+};
 
 export default { newPlot };
